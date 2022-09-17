e22c2078f4ec96257a2aef00223d7d15a1eb7bbb
serverless2021 <serverlesslowes2021@gmail.com>
2022-09-17 18:20:46 +0530

    version:4

--- file8.py
+++ file8.py
@@ -1,1 +1,4 @@
 echo "Git stash command hloding the current working environment without commit the changes ..."
+echo "push from local to remote is the below command"
+echo "git push origin <brname>
+
